0cabd8c76e98d1cb5644b90876a60a9db044730c
Walmik Deshpande <walmik.deshpande@gmail.com>
2021-04-20 21:31:20 -0700

    Add scale and chord methods

--- gen/index.js
+++ gen/index.js
@@ -1,6 +1,18 @@
+/**
+ * Tooling to generate simplified scale and chord map JSON files from tonal
+ * These are to be consumed by the exported Node module
+ */
+
 const fs = require('fs');
 const { Scale, Chord, ChordType } = require('@tonaljs/tonal');
 
+/**
+ *
+ * @param {*} param0
+ * @returns String
+ * Generate a bit map string denoting 1 for a notes to be used and 0 for a note
+ * to be skipped while traversing notes from a chromatic scale
+ */
 const getBitmap = ({ scale, chord }) => {
   const chromatic = Scale.get('C4 chromatic').notes.concat(
     Scale.get('C5 chromatic')
@@ -28,6 +40,7 @@ const getBitmap = ({ scale, chord }) => {
   return bitmap;
 };
 
+// Genarate JSON file for scaleMaps
 const scaleMaps = {};
 Scale.names().forEach((scale) => {
   scaleMaps[scale] = getBitmap({ scale });
@@ -41,6 +54,7 @@ fs.writeFile('./gen/scaleMaps.json', JSON.stringify(scaleMaps), function (err) {
   console.log('Generated scaleMaps.json');
 });
 
+// Genarate JSON file for chordMaps
 const numChords = ['4', '5', '6', '7', '9', '11', '13'];
 const chordMaps = {};
 ChordType.symbols().forEach((chord) => {
--- src/scalesAndChords.js
+++ src/scalesAndChords.js
@@ -1,7 +1,78 @@
 const scaleMaps = require('../gen/scaleMaps.json');
 const chordMaps = require('../gen/chordMaps.json');
 
-export const chords = () => [];
-export const scales = () => [];
-export const chord = () => [];
-export const scale = () => [];
+const getChromatic = (root, octave) => {
+  const o1 = [
+    'C',
+    'Db',
+    'D',
+    'Eb',
+    'E',
+    'F',
+    'Gb',
+    'G',
+    'Ab',
+    'A',
+    'Bb',
+    'B',
+  ].map((n) => n + octave);
+  const o2 = [
+    'C',
+    'Db',
+    'D',
+    'Eb',
+    'E',
+    'F',
+    'Gb',
+    'G',
+    'Ab',
+    'A',
+    'Bb',
+    'B',
+  ].map((n) => n + (octave + 1));
+
+  const c = o1.concat(o2);
+  return c.slice(c.indexOf(root + octave));
+};
+
+const _getNotesForScaleOrChord = ({ scale, chord }) => {
+  const rootOctaveScale = scale || chord;
+  const NOTES_TYPE = scale ? 'scale' : 'chord';
+  if (typeof rootOctaveScale !== 'string') {
+    throw `${rootOctaveScale} is not a valid input for ${NOTES_TYPE}`;
+  }
+  const indexOfFirstSpace = rootOctaveScale.indexOf(' ');
+  const scaleOrChord = rootOctaveScale.slice(indexOfFirstSpace + 1);
+  const rootOctave = rootOctaveScale.slice(0, indexOfFirstSpace);
+  const root = rootOctave.replace(/\d/g, '');
+  const octave = +rootOctave.replace(/\D/g, '');
+
+  if (isNaN(octave)) {
+    throw `${rootOctave[0]} does not have a valid octave`;
+  }
+
+  if (!scaleMaps[scaleOrChord] && !chordMaps[scaleOrChord]) {
+    throw `${rootOctaveScale} is not a valid ${NOTES_TYPE}`;
+  }
+  const chroma = getChromatic(root, octave);
+  const acc = [];
+  let p1 = 0,
+    p2 = 0;
+
+  const map = scale ? scaleMaps : chordMaps;
+
+  while (p1 < map[scaleOrChord].length) {
+    if (map[scaleOrChord][p1] === '1') {
+      acc.push(chroma[p2]);
+    }
+    p1++;
+    p2++;
+  }
+
+  return acc;
+};
+
+export const chords = () => Object.keys(chordMaps);
+export const scales = () => Object.keys(scaleMaps);
+export const chord = (chord) => _getNotesForScaleOrChord({ chord });
+export const scale = (scale) => _getNotesForScaleOrChord({ scale });
